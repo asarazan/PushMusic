341200c72ec60b96f88c908ba22000f45f7c09e0
Aaron Sarazan <aaron@sarazan.net>
2010-12-31 11:34:26 -0800

    blank project

---
+++ Classes/PushMusicAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PushMusicAppDelegate.h
+//  PushMusic
+//
+//  Created by Aaron Sarazan on 12/31/10.
+//  Copyright 2010 Spark Plug Games, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PushMusicAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
